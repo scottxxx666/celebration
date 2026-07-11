d5f09d76b9b6dd3241d5ec46843bc061fe92c8ec
scottxxx666 <scottxxx666@gmail.com>
2026-07-11 14:43:33 +0800

    chore: set to host mode to let mobile test

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './',
+  server: {
+    host: true, // or '0.0.0.0'
+    port: 3000, // optional: fix port number
+  },
 });
